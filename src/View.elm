5e5638d43fa60b77e5d13675b1aa518df1c9c32c
dhershman1 <dhershman@budgetdumpster.com>
2019-05-03 16:55:14 -0400

    Commenting out the grid elm for now (may become board)

--- src/View.elm
+++ src/View.elm
@@ -1,25 +1,10 @@
 module View exposing (view)
 
 import Cell exposing (Cell)
-import Grid exposing (Grid, encode, initialize, toList)
 import Html exposing (Html, aside, button, div, footer, h1, header, main_, span, text)
 import Html.Attributes exposing (attribute, class, classList, href, id)
 import Messages exposing (Msg(..))
 import Model exposing (Model)
-
-
-cellToAttrs : Cell -> List (Html.Attribute msg)
-cellToAttrs c =
-    let
-        cec =
-            Cell.toRecord c
-    in
-    [ attribute "x" (String.fromInt (Tuple.first cec.pos)), attribute "y" (String.fromInt (Tuple.second cec.pos)) ]
-
-
-viewCells : List Cell -> List (Html msg)
-viewCells cells =
-    List.map (\c -> span (List.concat [ [ class "grid__cell" ], cellToAttrs c ]) [ text (Cell.toRecord c).char ]) cells
 
 
 view : Model -> Html Msg
@@ -27,5 +12,5 @@ view model =
     main_ [ class "tiier" ]
         [ header [] [ h1 [] [ text "Tiier" ] ]
         , aside [ class "details" ] [ span [ class "location" ] [ text "Location: " ], span [] [ text "Nowhere" ] ]
-        , div [ class "grid" ] (viewCells (toList (initialize 50 40)))
+        , div [ class "grid" ] [ text "Grid!" ])
         ]
